adc738fab90f75fd45cf26f95d4323440979253c
majidgeeks <majidrajput246@gmail.com>
2023-08-31 01:10:02 +0500

    practise exercises javaScript

---
+++ app.js
@@ -0,0 +1,63 @@
+
+// VARIABLES	FOR	NUMBERS
+
+// 1. Declare a variable called age & assign to it your age. Show 
+// your age in an alert box.
+
+// ans:// var age = 34;
+// alert("i am "+ age + " years old");
+
+// 2. Declare & initialize a variable to keep track of how many 
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You 
+// have visited this site N times”.
+
+// var visitedSite = 44;
+// alert("you have visited this site "+ visitedSite + " times")
+
+
+// 3. Declare a variable called birthYear & assign to it your 
+// birth year. Show the following message in your browser:
+
+// var birthdayYear = 1988;
+// document.write("my bithday year is " + birthdayYear);
+
+
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables 
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to 
+// order
+// Show the following message in your browser: “John 
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// var visitorName = "Majid Hussain";
+// var ProductTitle = "pants";
+// var Quantity = 5;
+// var storeName = "chase Value";
+// document.write(visitorName + " orderd " + Quantity + ProductTitle + " on " + storeName);
+
+// end
+
+// VARIABLE NAMES:	LEGAL	&	ILLEGAL
+
+// 1. Declare 3 variables in one statement.
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+
+// 3. Display this in your browser
+// a. A heading stating “Rules for naming JS variables”
+// b. Variable names can only contain ______, ______, 
+// ______ and ______. 
+// For example $my_1stVariable
+// c. Variables must begin with a ______, ______ or 
+// _____. For example $name, _name or name
+// d. Variable names are case _________
+// e. Variable names should not be JS _________
+
+
+
